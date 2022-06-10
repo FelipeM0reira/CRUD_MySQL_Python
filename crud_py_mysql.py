8da910f371554770d36c22c99353a7968a0cd7b1
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2022-06-10 03:07:56 -0300

    crud ok!

--- crud_py_mysql.py
+++ crud_py_mysql.py
@@ -18,6 +18,33 @@ resultado = cursor.fetchall() # ler o banco de dados.
 """
 # CRUD
 
+# Creat
+nome_filme = "Jhon Wick"
+nota_filme = 9.2
+comando = f'INSERT INTO filmes (nome_filme, nota_filme) VALUES ("{nome_filme}", {nota_filme})'
+cursor.execute(comando)
+conexao.commit()
+
+
+# Read
+comando = f'SELECT * FROM filmes'
+cursor.execute(comando)
+resultado = cursor.fetchall()
+print(resultado)
+
+# Update
+nome_filme = "Jhon Wick"
+nota_filme = 9.3
+comando = f'UPDATE filmes SET nota_filme = {nota_filme} WHERE nome_filme = "{nome_filme}"'
+cursor.execute(comando)
+conexao.commit()
+
+# Delete
+nome_filme = ""
+comando = f'DELETE FROM filmes WHERE nome_filme = "{nome_filme}"'
+cursor.execute(comando)
+conexao.commit()
+
 
 # Fecha a conexão
 cursor.close()
